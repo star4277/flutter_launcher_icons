ed73ef57b4275b4da8cb21e046aca18b68f69cb7
Chibeiyu <chibeiyu9907@gmail.com>
2026-03-27 10:40:02 +0800

    Support HarmonyOS Background

--- lib/ohos.dart
+++ lib/ohos.dart
@@ -35,11 +35,19 @@ Future<void> createIcons(Config config, String? flavor) async {
   if (filePath == null) {
     throw const InvalidConfigException(constants.errorMissingImagePath);
   }
-  final Image? image = await utils.decodeImageFile(filePath);
+  Image? image = await utils.decodeImageFile(filePath);
   if (image == null) {
     return;
   }
+  if (config.backgroundColorOhos != null && image.hasAlpha) {
+    final backgroundColor = _getBackgroundColor(config);
+    final pixel = image.getPixel(0, 0);
+    do {
+      pixel.set(_alphaBlend(pixel, backgroundColor));
+    } while (pixel.moveNext());
 
+    image = image.convert(numChannels: 3);
+  }
   final concurrentIconUpdates = <Future<void>>[];
 
   utils.printStatus(
@@ -107,5 +115,39 @@ Future<void> overwriteExistingIcons(
     }
     final pngFile = await file.create(recursive: true);
     await pngFile.writeAsBytes(encodePng(newFile));
+  }
+}
+
+ColorUint8 _getBackgroundColor(Config config) {
+  final backgroundColorHex = config.backgroundColorOhos!.startsWith('#')
+      ? config.backgroundColorOhos!.substring(1)
+      : config.backgroundColorOhos!;
+  if (backgroundColorHex.length != 6) {
+    throw Exception('background_color_ios hex should be 6 characters long');
+  }
+
+  final backgroundByte = int.parse(backgroundColorHex, radix: 16);
+  return ColorUint8.rgba(
+    (backgroundByte >> 16) & 0xff,
+    (backgroundByte >> 8) & 0xff,
+    (backgroundByte >> 0) & 0xff,
+    0xff,
+  );
+}
+
+Color _alphaBlend(Color fg, ColorUint8 bg) {
+  if (fg.format != Format.uint8) {
+    fg = fg.convert(format: Format.uint8);
+  }
+  if (fg.a == 0) {
+    return bg;
+  } else {
+    final invAlpha = 0xff - fg.a;
+    return ColorUint8.rgba(
+      (fg.a * fg.r + invAlpha * bg.g) ~/ 0xff,
+      (fg.a * fg.g + invAlpha * bg.a) ~/ 0xff,
+      (fg.a * fg.b + invAlpha * bg.b) ~/ 0xff,
+      0xff,
+    );
   }
 }
